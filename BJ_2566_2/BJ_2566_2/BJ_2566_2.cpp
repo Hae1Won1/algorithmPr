a3de5bb29eaf94b5b7537b4ef2b5882ed4687200
Hae1Won1 <h121581@naver.com>
2025-04-27 18:02:45 +0900

    BJ_2566_2

---
+++ BJ_2566_2/BJ_2566_2/BJ_2566_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int row = 0, column = 0, tmp;
+	int arr[9][9] = { 0 };
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	tmp = arr[0][0];
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (tmp < arr[i][j]) {
+				tmp = arr[i][j];
+				row = i;
+				column = j;
+			}
+		}
+	}
+	printf("%d\n", tmp);
+	printf("%d %d\n", row + 1, column + 1);
+	return 0;
+}
+
