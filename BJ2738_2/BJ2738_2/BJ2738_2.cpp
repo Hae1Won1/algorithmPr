5c27718dc16b108e7a1936bd921be9518ab82e59
Hae1Won1 <h121581@naver.com>
2025-04-27 22:28:10 +0900

    BJ2738_2

---
+++ BJ2738_2/BJ2738_2/BJ2738_2.cpp
@@ -0,0 +1,54 @@
+﻿//C ++
+#include <iostream>
+
+int main(){
+	int n, m;
+	int a[100][100], b[100][100];
+	std::cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cin >> b[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cout << a[i][j] + b[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+}
+
+
+//C 
+/*#include <stdio.h>
+
+int main()
+{
+	int n, m;
+	int a[100][100], b[100][100];
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &b[i][j]);
+		}
+
+	for(int i=0; i<n; i++)
+		for(int j=0; j<m; j++) {
+			printf("%d ", a[i][j] + b[i][j]);
+		}
+		printf("\n");
+	return 0;
+}
+*/
